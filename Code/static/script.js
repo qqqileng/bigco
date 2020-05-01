e25ff20a40963971f11dac2500f30db920075aea
ababushkin6 <54685325+ababushkin6@users.noreply.github.com>
2020-05-01 09:38:12 -0400

    Add files via upload

--- Code/static/script.js
+++ Code/static/script.js
@@ -1,6 +1,6 @@
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash = (0xFFFFFF).toString(16); //Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
 
